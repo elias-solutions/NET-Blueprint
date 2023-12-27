fab93e1d49efb0bb06a2e9df1b58191505bc3b88
Jonas Elias <jonas.elias@bit.admin.ch>
2023-12-27 08:51:55 +0100

    cleanup

--- NET.Backend.Blueprint.Integration.xUnit.Tests.SQlite/Environment/SharedTestCollection.cs
+++
@@ -1,9 +0,0 @@
-﻿using Xunit;
-
-namespace NET.Backend.Blueprint.Integration.xUnit.Tests.SQlite.Environment
-{
-    [CollectionDefinition(nameof(SharedTestCollection))]
-    public class SharedTestCollection : ICollectionFixture<IntegrationTestFixture>
-    {
-    }
-}
--- NET.Backend.Blueprint.Integration.xUnit.Tests.SQlite/Environment/TestUsers.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NET.Backend.Blueprint.Integration.xUnit.Tests.SQlite.Environment
-{
-    public static class TestUsers
-    {
-        public static User Admin { get; } = new (Guid.Parse("E3D3D338-2053-4CE9-877D-DE4563FE321C"), Roles.Admin, $"{Roles.Admin}@{Roles.Admin}.ch", new[] { Roles.Admin });
-
-        public static User Standard { get; } = new(Guid.Parse("E3D3D338-2053-4CE9-877D-DE4563FE321D"), Roles.Standard, $"{Roles.Standard}@{Roles.Standard}.ch", new[] { Roles.Standard });
-    }
-}
